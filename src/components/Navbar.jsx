7f6953be6aef5e177820889f955e78b8d34b7919
nebo1991 <nebojsa.kovacevic1991@gmail.com>
2024-10-17 20:47:29 +0200

    fix: refactoring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
+import { Link } from 'react-router-dom';
 import recipesLogo from '../assets/recipes-logo.svg'
 
 const Navbar = () => {
     return (
         <div className="flex justify-between items-center p-4 bg-gradient-to-r from-[#3a1c71] via-[#d76d77] to-[#ffaf7b] text-white ">
-            <img id="logo" src={recipesLogo} alt="logo-photo" className='w-10' />
-            <button>About</button>
+            <Link to="/"> <img id="logo" src={recipesLogo} alt="logo-photo" className='w-10' /> </Link>
+            <Link to="/about"><button>About</button></Link>
         </div>
+       
+    
+
     )
 }
 
