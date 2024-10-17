a0ae01e126fe565302e13ece16488a99aada7d97
nebo1991 <nebojsa.kovacevic1991@gmail.com>
2024-10-17 15:58:52 +0200

    fix: move state from component level to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,57 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './components/Footer'
-import Navbar from './components/Navbar'
-import RecipesListPage from './components/RecipesListPage'
-import RecipeDetailsCard from './components/RecipeDetailsCard'
+// src/App.jsx
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import RecipesListPage from './components/RecipesListPage';
+import RecipeDetailsCard from './components/RecipeDetailsCard';
+import { useState } from 'react';
+import recipesData from './assets/recipes.json';
 
 function App() {
-  return (
-    <div className="flex flex-col min-h-screen"> 
-      <Navbar />
-      <main className="flex-grow"> 
-        <Routes>
-          <Route path="/" element={<RecipesListPage />} />
-          <Route path="/recipes/:id" element={<RecipeDetailsCard />} />
-        </Routes>
-      </main>
-      <Footer /> 
-    </div>
-  )
+    const [recipes, setRecipes] = useState(recipesData);
+    const [name, setName] = useState("");
+    const [calories, setCalories] = useState(0);
+    const [servings, setServings] = useState(1);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newRecipe = {
+            id: Math.floor(Math.random() * (90 - 10 + 1)) + 10,
+            name,
+            calories,
+            image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpngtree.com%2Fso%2Ffood-svg&psig=AOvVaw0Svct_7bgRCbzn-Skr4Izx&ust=1729259421085000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLDkmb_HlYkDFQAAAAAdAAAAABAE", 
+            servings,
+        };
+
+        setRecipes((prevRecipes) => [...prevRecipes, newRecipe]);
+        setName("");
+        setCalories(0);
+        setServings(1);
+    };
+
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-grow">
+                <Routes>
+                    <Route path="/" element={
+                        <RecipesListPage 
+                            recipes={recipes} 
+                            name={name} 
+                            calories={calories} 
+                            setName={setName} 
+                            setCalories={setCalories} 
+                            setServings={setServings} 
+                            handleSubmit={handleSubmit} 
+                        />
+                    } />
+                    <Route path="/recipes/:id" element={<RecipeDetailsCard recipes={recipes} />} />
+                </Routes>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
