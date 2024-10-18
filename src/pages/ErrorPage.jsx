ab392618feab7ab838604e88360dd1972a5f133a
nebo1991 <nebojsa.kovacevic1991@gmail.com>
2024-10-18 10:39:00 +0200

    fix: test

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom';
 import errorPageIcon from '../assets/error-page-icon.svg'
 
 const ErrorPage = () => {
     return(
         <>
+        <Link to="/*">
         <div className="flex flex-col items-center justify-center">
         <img className='w-[100px] my-16' src={errorPageIcon}/>
         <h1 >Ops....</h1>
         <p className='my-2'>This page does not exist</p>
         </div>
+        </Link>
         </>
     )
 }
